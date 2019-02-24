f260ada261b63cb2335c9e27f24ce709732cd3dd
dlee67 <dlee67@msudenver.edu>
2019-02-24 10:10:58 -0700

    Ready to present.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Dad, where is my Halo 6?
-Son, go ride some bike.
+# WillBikeForGame is ...
+a project which aims to encourage students in Elementary to Middle school to maintain
+their balance of health and gaming.
+
+# This is accomplished by ...
+having two participants for the application, where one participants
+keeps track of the rewards the young individual could be awarded with
+after spending sometime biking.
--- app/src/main/java/com/less/TaTiTuTeTor/ParentActivity.kt
+++ app/src/main/java/com/less/TaTiTuTeTor/ParentActivity.kt
@@ -2,9 +2,11 @@ package com.less.TaTiTuTeTor
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -12,24 +14,26 @@ import java.io.File
 
 class ParentActivity : AppCompatActivity(){
 
-    lateinit var database: DatabaseReference
     lateinit var storageRef: StorageReference
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.for_parents)
 
-        var imageView = findViewById<ImageView>(R.id.child_picture)
+        imageView = findViewById<ImageView>(R.id.child_picture)
 
-        database = FirebaseDatabase.getInstance().getReference()
-        database.addValueEventListener(object : ValueEventListener {
+        FirebaseDatabase.getInstance().getReferenceFromUrl("https://tatitutetor.firebaseio.com/").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                imageView.setImageResource(android.R.color.transparent)
+                Toast.makeText(applicationContext, "Your child has finished a task!", Toast.LENGTH_LONG)
                 var localFile = File.createTempFile("images", "png");
                 val value = dataSnapshot.getValue() as List<Map<*, *>>
+                Thread.sleep(2000)
                 for(task in value){
                     var isDone = task.get("done")
                     Log.i("dhl", "isDone is: " + isDone.toString());
-                    Log.i("dhl", "where the full URL is: " + "gs://tatitutetor.appspot.com/" + isDone.toString() + ".png")
+                    Log.i("dhl", "where the full URL is: " + "gs://tatitutetor.appspot.com/" + isDone.toString())
                     if(!isDone!!.equals("false")){
                         Log.i("dhl", "In the block of is not false");
                         storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://tatitutetor.appspot.com/").child(isDone.toString())
